407785a97da2af0b890bcd58e7fc56c0bfc6c14a
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-08-17 04:21:38 +0530

    Committed Add and Sub functionality to Branch_A

--- src/Calci/Calculator.java
+++ src/Calci/Calculator.java
@@ -3,8 +3,10 @@ package Calci;
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Add a = new Add();
+		Sub s = new Sub();
+		a.Addd(10, 20);
+		s.Subbb(20,10);
 	}
 
 }
